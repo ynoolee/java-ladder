4bad9617f981bac16a450c53302610f255ab988c
ynoolee <heerarea129@gmail.com>
2025-01-26 21:05:58 +0900

    refactor : 람다를 활용해 중복 제거하기

--- src/main/java/nextstep/fp/Lambda.java
+++ src/main/java/nextstep/fp/Lambda.java
@@ -26,31 +26,13 @@ public class Lambda {
         }).start();
     }
 
-    public static int sumAll(List<Integer> numbers) {
-        int total = 0;
-        for (int number : numbers) {
-            total += number;
-        }
-        return total;
+    public interface NumberConditional {
+        boolean isSatisfied(int number);
     }
 
-    public static int sumAllEven(List<Integer> numbers) {
-        int total = 0;
-        for (int number : numbers) {
-            if (number % 2 == 0) {
-                total += number;
-            }
-        }
-        return total;
-    }
-
-    public static int sumAllOverThree(List<Integer> numbers) {
-        int total = 0;
-        for (int number : numbers) {
-            if (number > 3) {
-                total += number;
-            }
-        }
-        return total;
+    public static int sumOnlyConditionSatisfiedNumbers(List<Integer> numbers, NumberConditional condition) {
+        return numbers.stream()
+            .filter(condition::isSatisfied)
+            .reduce(0, Integer::sum);
     }
 }
